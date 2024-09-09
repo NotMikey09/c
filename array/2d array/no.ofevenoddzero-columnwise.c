321a81d0243e007e0206a0a97655ca9c1438391b
Pratyush Mishra <pratyushmishra975@gmail.com>
2024-09-10 00:11:31 +0530

    2d array udated

---
+++ array/2d array/no.ofevenoddzero-columnwise.c
@@ -0,0 +1,39 @@
+ /* ********* Finding the no. of even/odd/zero elements row wise *********
+ * 
+ * This program read elements of n*n matrix and find the no. of even/odd/zero elements column wise
+ */
+
+#include <stdio.h>
+ int main()
+ {
+     int a[10][10], i, j, nr, nc, even, odd, zero;
+     printf("Enter the number of rows and columns of the matrix: ");
+     scanf("%d %d", &nr, &nc);
+     printf("Enter the %d elements of the matrix:\n",nr*nc);
+     for(i=0; i<nr; i++)
+     {
+        for(j=0; j<nc; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+     }
+     printf("\t Even\todd\tzero");
+     printf("\n----------------------------------------------------------");
+     for(j=0; j<nc;j++)
+     {
+        for(even=odd=zero=i=0;i<nr;i++)
+         {
+             if(a[i][j]==0)
+             {
+                zero++;
+             }
+             else if(a[i][j]%2==0)
+             {
+                even++;
+             }
+             else odd++;
+         }
+         printf("\ncolumn %d:\t%d\t%d\t%d", i+1, even,odd,zero);
+     }
+      return 0;
+ }
